b476dcf0f9787dea23520682c620a4db1397e5bc
bentona <benton.anderson@gmail.com>
2015-01-22 17:06:42 -0500

    update script to use forever, remove broken ambush

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 #you should have these somewhere, filled out
 #export HUBOT_HIPCHAT_JID=
 #export HUBOT_HIPCHAT_PASSWORD=
+forever start -c coffee node_modules/.bin/hubot -a hipchat
 
-./bin/hubot -a hipchat -n r2
